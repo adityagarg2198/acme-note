52ff3364e410416f0242f24dcbb33a4e1426bcf5
Aditya Garg <adityagarg@Adityas-MacBook-Air.local>
2024-04-13 19:48:22 +0530

    pointer added

--- src/Pages/Footer/Footer.component.tsx
+++ src/Pages/Footer/Footer.component.tsx
@@ -77,18 +77,18 @@ const Footer = () => {
           <div className='flex md:gap-10 items-center'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
-              className='icon icon-tabler icon-tabler-brand-facebook stroke-indigo-400 w-20 h-20'
+              className='icon icon-tabler icon-tabler-brand-facebook stroke-indigo-400 w-20 h-20 cursor-pointer'
               viewBox='0 0 24 24'
               strokeWidth='1.8'
               fill='none'
               strokeLinecap='round'
               strokeLinejoin='round'>
               <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-              <path d='M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3' />
+              <path d='M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3'/>
             </svg>
             <svg
               xmlns='http://www.w3.org/2000/svg'
-              className='icon icon-tabler icon-tabler-brand-instagram stroke-indigo-400 w-20 h-20'
+              className='icon icon-tabler icon-tabler-brand-instagram stroke-indigo-400 w-20 h-20 cursor-pointer'
               viewBox='0 0 24 24'
               strokeWidth='1.8'
               fill='none'
@@ -101,7 +101,7 @@ const Footer = () => {
             </svg>
             <svg
               xmlns='http://www.w3.org/2000/svg'
-              className='icon icon-tabler icon-tabler-brand-x stroke-indigo-400 w-20 h-20'
+              className='icon icon-tabler icon-tabler-brand-x stroke-indigo-400 w-20 h-20 cursor-pointer'
               viewBox='0 0 24 24'
               strokeWidth='1.8'
               fill='none'
